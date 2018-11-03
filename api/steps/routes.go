70e39c99ebd99b7ad11de7909a7a8cd66785c1fc
Nick Shepherd <goodcodeguy@gmail.com>
2018-11-02 23:43:41 -0400

    add initial routes for steps, users and workspaces

---
+++ api/steps/routes.go
@@ -0,0 +1,14 @@
+package steps
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/goodcodeguy/bottomline/lib/routes"
+)
+
+func Routes() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Get("/", routes.RouteNotImplemented)
+
+	return router
+}
